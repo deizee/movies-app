4510f70bc9d022a21f1820944af5f62e00cc1a8c
deizee <deizee@mail.ru>
2021-02-03 13:49:09 +0500

    Render background poster

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <MoviesList :list="moviesList" />
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList";
+import PosterBg from "@/components/PosterBg";
 
 export default {
   name: "App",
   components: {
-    MoviesList
+    MoviesList,
+    PosterBg
+  },
+  data() {
+    return {
+      posterBg: ""
+    };
   },
   computed: {
     ...mapGetters("movies", ["moviesList"])
   },
   methods: {
-    ...mapActions("movies", ["fetchMovies"])
+    ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    }
   }
 };
 </script>
@@ -27,5 +38,6 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  position: relative;
 }
 </style>
